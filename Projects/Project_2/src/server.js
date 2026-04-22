1b203a15637b77c4fcd537ce1ba28b28141c0e7c
Kayongo Emmanuel <kayongoemma3@gmail.com>
2026-04-22 17:50:14 +0300

    Send a Website to the Root directory request.

--- Projects/Project_2/src/server.js
+++ Projects/Project_2/src/server.js
@@ -1,11 +1,32 @@
 // importing the express framework
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 // Create an instance for that imported express framework
 const app = express();
 
 // Define a port
 const PORT = process.env.PORT || 3000;
+
+// Get the filename of the current file
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the foldername of the current file
+const __dirname = dirname(__filename);
+
+// Relate the public directory to the current directory
+app.use(express.static(path.join(__dirname,"../Public")));
+
+// Enable our app handle json files
+app.use(express.json());
+
+// Interprete the get request at the root directory
+app.get("/", (req,res) => {
+    console.log("Response at the root directory has been detected!");
+    const file = path.join(__dirname,"Public","index.html");
+    res.sendFile(file);
+});
 
 
 // Let our app listen to whatever happens at the port
