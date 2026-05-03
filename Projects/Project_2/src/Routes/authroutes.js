ec4c5a453350c8b19cde90c938217f5607d61734
Kayongo Emmanuel <kayongoemma3@gmail.com>
2026-05-03 09:53:43 +0300

    Create register/login endpoints with hashed passwords, JWT auth, and default todo creation.

--- Projects/Project_2/src/Routes/authroutes.js
+++ Projects/Project_2/src/Routes/authroutes.js
@@ -19,7 +19,7 @@ router1.post("/register", (req,res) => {
         // Prepare the sql query
         const insert = db.prepare(`
             INSERT INTO user (username, password)
-            VALUE (?,?)   
+            VALUES (?,?)   
         `);
         // Add the Values into the query
         const add = insert.run(username, encrypt);
@@ -27,24 +27,69 @@ router1.post("/register", (req,res) => {
         // Create a default todo
         const todo = db.prepare(`
             INSERT INTO todos (user_id,todo)
-            VALUE (?,?)
+            VALUES (?,?)
         `);
-
         // Add values in the todo
+        const DefaultTodo = "Welcome to your todo list! This is a default todo. You can delete it if you want.";
         const add1 = todo.run(add.lastInsertRowid,DefaultTodo);
 
-        res.sendStatus(200);
+        // Adding the token
+        const token = jwt.sign({ id: add.lastInsertRowid },process.env.JwT_SECRETKEY, {expiresIn: '24h' });
+
+        // Send the token to the user
+        // This makes our application fast
+        res.json({ token });
+
+        res.sendStatus(200);    
     } catch (err) {
         console.log(err.message);
         res.sendStatus(503);
      };
     
-    });     
+    });   
+      
 // Handle a post request at the login
 router1.post("/login", (req,res) => {
+    // Get the username and password from the request body
     const { username, password } = req.body;
-    console.log(username, password);
-    res.sendStatus(200);
+
+    try {
+        // Select the user from the database
+        const db_user = db.prepare(`
+            SELECT * FROM user WHERE username = ?
+        `);
+
+        // Get the user from the database
+        const get_user = db_user.get(username);
+
+        // 1. Check if user exists
+if (!get_user) {
+    return res.status(404).send({ message: "User not found" });
+}
+
+// 2. Compare passwords (now that we know the user exists)
+const compare = bcrypt.compareSync(password, get_user.password);
+
+if (!compare) {
+    // 3. Use return to stop execution if password is wrong
+    return res.status(401).send({ message: "Invalid password!" });
+}
+
+// 4. If we reached here, password is correct. 
+// Match your variable name (token) with what you send in res.json
+const token = jwt.sign(
+    { id: get_user.id }, 
+    process.env.JwT_SECRETKEY, 
+    { expiresIn: "24h" }
+);
+
+// Use 'token' here because that's what you named it above
+return res.json({ token });
+        
+    } catch (error) {
+      console.log(error.message);
+      res.sendStatus(503);  
+    };
 });
 
 // Export the instance to be used in other files
